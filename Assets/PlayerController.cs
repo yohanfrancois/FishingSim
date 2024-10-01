8356079d74c16c432e68896f17d612ab50218249
Steeven Novo <steeven_yugo@hotmail.fr>
2024-10-01 16:43:11 +0200

    Add throw charge

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,6 +9,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float arrowRotateSpeed;
     [SerializeField] private float aimAngleMin;
     [SerializeField] private float aimAngleMax;
+    [SerializeField] private GameObject fishBait;
+    [SerializeField] private float throwForce;
+    [SerializeField] private float throwChargeSpeed;
+    [SerializeField] private GameObject throwChargeBarGO;
+    [SerializeField] private Image throwChargeBar;
+
+    private bool isChargingThrow = false;
+    private bool isChargingUp = true;
+    private float throwCharge = 0f;
+
+    private void Start()
+    {
+        throwChargeBarGO.SetActive(false);
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -16,7 +32,6 @@ public class PlayerController : MonoBehaviour
         if (currentZRotation > 180f)
             currentZRotation -= 360f;
 
-        Debug.Log(currentZRotation);
         if (Input.GetKey(KeyCode.A))
         {
             // Calculez la nouvelle rotation en ajoutant l'entrée utilisateur
@@ -37,5 +52,57 @@ public class PlayerController : MonoBehaviour
 
             arrowDir.rotation = Quaternion.Euler(0f, 180f, newZRotation);
         }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ThrowFishBait();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            throwChargeBarGO.SetActive(true);
+            throwChargeBar.fillAmount = 0f;
+            isChargingThrow = true;
+            throwCharge = 0f;
+            isChargingUp = true;
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            isChargingThrow = false;
+            ThrowFishBait();
+            throwChargeBarGO.SetActive(false);
+            throwCharge = 0f;
+        }
+        if (isChargingThrow)
+        {
+            if (isChargingUp)
+            {
+                throwCharge += throwChargeSpeed * Time.deltaTime;
+                if (throwCharge > 100f)
+                {
+                    throwCharge = 100f;
+                    isChargingUp = false;
+                }
+            }
+            else
+            {
+                throwCharge -= throwChargeSpeed * Time.deltaTime;
+                if (throwCharge < 0f)
+                {
+                    throwCharge = 0f;
+                    isChargingUp = true;
+                }
+            }
+
+            throwChargeBar.fillAmount = throwCharge / 100f;
+        }
+
+    }
+
+    private void ThrowFishBait()
+    {
+        fishBait.transform.localPosition = Vector3.zero;
+        fishBait.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        fishBait.GetComponent<Rigidbody2D>().AddForce(arrowDir.up * (throwForce * (throwCharge / 100f)) );
     }
 }
